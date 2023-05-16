7628bb328b89c646639ae94192f79900c113fa5c
hrishikesh <hrishikesh.muley14@gmail.com>
2023-05-16 21:42:18 +0530

    adding web data model. closes #2

---
+++ src/main/java/guru/springframework/msscbeerservice/model/BeerStyleEnum.java
@@ -0,0 +1,8 @@
+package guru.springframework.msscbeerservice.model;
+
+/**
+ * @Author Hrishikesh Muley
+ */
+public enum BeerStyleEnum {
+    LAGER,PILSNER,STOUT,GOSE,PORTER,ALE,WHEAT,IPA,PALE_ALE,SAISON
+}
